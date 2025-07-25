99891443837c2cb9d94c4463c38cda20045a0bfe
Kodewerfer <lijleen@gmail.com>
2025-07-25 16:43:20 -0400

    base conversion of sidebar to use shadcn component

--- src/app/[lang]/users/layout.tsx
+++ src/app/[lang]/users/layout.tsx
@@ -1,15 +1,17 @@
 import React from "react";
 import UserCenterSideBar from "@/components/users/LayoutSideBar";
 
+import {SidebarProvider, SidebarTrigger} from "@/components/ui/sidebar"
+
 export default async function UserCenterLayout({children,}: Readonly<{
     children: React.ReactNode;
 }>) {
     return (
         <div className="flex w-full h-screen bg-primary-50">
-            
-            <UserCenterSideBar/>
-            
-            {children}
+            <SidebarProvider defaultOpen={true}>
+                <UserCenterSideBar/>
+                {children}
+            </SidebarProvider>
         
         </div>
     )
--- src/components/users/LayoutSideBar.tsx
+++ src/components/users/LayoutSideBar.tsx
@@ -1,11 +1,22 @@
 'use client'
 
 import React, {ComponentType, useCallback, useState} from "react";
-import {ChartPieIcon, Cog8ToothIcon, UserCircleIcon, WalletIcon} from "@heroicons/react/16/solid";
+import {
+    AdjustmentsHorizontalIcon,
+    ChartPieIcon,
+    Cog8ToothIcon,
+    UserCircleIcon,
+    WalletIcon
+} from "@heroicons/react/16/solid";
 import Image from "next/image";
 import Link from "next/link";
+// shadcn sidebar
+import {
+    Sidebar, SidebarContent, SidebarFooter, SidebarGroup, SidebarGroupContent,
+    SidebarHeader, SidebarMenu, SidebarRail, SidebarMenuItem, SidebarMenuSkeleton, SidebarMenuButton, useSidebar
+} from "@/components/ui/sidebar"
 
-
+// static mockup
 const ALL_SIDEBAR_OPTIONS = [
     {
         icon: UserCircleIcon,
@@ -35,55 +46,91 @@ export default function UserCenterSideBar() {
     }, []);
     
     return (
-        <div className="sidebar bg-primary-900 text-white w-64 min-h-screen flex flex-col">
-            <div className="p-6">
-                <div className="flex items-center space-x-3">
-                    <div className="bg-highlight-500 w-10 h-10 rounded-full flex items-center justify-center">
-                        <span className="font-black text-white text-xs">LOGO</span>
+        <Sidebar collapsible={'icon'}>
+            <SidebarHeader className={'bg-primary-800 text-primary-200'}>
+                <div className="flex grow py-6 items-center justify-center">
+                    
+                    <div
+                        className={`bg-highlight-500 size-12 shrink-0 rounded-full flex items-center justify-center`}>
+                        <span className="font-black text-primary-200 text-xs">LOGO</span>
                     </div>
-                    <div>
-                        <h1 className="text-xl font-bold">User Center</h1>
+                
+                </div>
+            </SidebarHeader>
+            <SidebarContent className={'bg-primary-800 text-primary-200'}>
+                <SidebarGroup>
+                    {/*<SidebarGroupLabel className={'text-primary-200'}>User</SidebarGroupLabel>*/}
+                    <SidebarGroupContent>
+                        <SidebarMenu>
+                            
+                            <React.Suspense fallback={<SidebarMenuSkeleton showIcon/>}>
+                                <SideBarContent activeItemLabel={activeItemLabel} onItemClick={handleSwitchingItem}/>
+                            </React.Suspense>
+                        
+                        </SidebarMenu>
+                    </SidebarGroupContent>
+                </SidebarGroup>
+            </SidebarContent>
+            <SidebarFooter className={'bg-primary-800 text-primary-200'}>
+                <div className="p-4 border-t border-primary-700">
+                    <div className="flex items-center">
+                        
+                        <Image src="https://randomuser.me/api/portraits/women/46.jpg"
+                               alt="Admin"
+                               quality={85}
+                               width={120}
+                               height={120}
+                               className="w-10 h-10 rounded-full"/>
+                        
+                        <div className="ml-3">
+                            <p className="font-medium">Current User</p>
+                            <p className="text-sm text-primary-400">123@312.com</p>
+                        </div>
+                        
+                        <a href="#" className="ml-auto text-primary-400 hover:text-primary-200">
+                            <AdjustmentsHorizontalIcon className={"size-6"}/>
+                        </a>
+                    
                     </div>
                 </div>
-            </div>
-            
-            <nav className="flex-1 mt-6">
-                <ul className="space-y-1 px-4">
-                    {ALL_SIDEBAR_OPTIONS.map((option, index) => (
+            </SidebarFooter>
+            <SidebarRail
+                className={"hover:bg-gradient-to-l hover:from-secondary-400 hover:to-primary-800"}/>
+        </Sidebar>
+    )
+}
+
+/**
+ * If the options are fetched from server, this component will handle the data fetching
+ * @param activeItemLabel
+ * @param onItemClick
+ */
+function SideBarContent({activeItemLabel, onItemClick}: {
+    activeItemLabel: string;
+    onItemClick: (ev: React.PointerEvent, label: string) => void;
+}) {
+    
+    const [sidebarOptions] = useState(ALL_SIDEBAR_OPTIONS); //fetch or useSWR() if from server
+    
+    return (
+        <>
+            {sidebarOptions.map((option, index) => (
+                
+                <SidebarMenuItem key={index}>
+                    <SidebarMenuButton asChild>
                         <SideBarItems
-                            key={index}
                             bIsActive={activeItemLabel === option.label}
                             ItemIcon={option.icon}
                             itemLabel={option.label}
-                            OnClick={(ev, label) => handleSwitchingItem(ev, label)}
+                            OnClick={(ev, label) => onItemClick(ev, label)}
                         />
-                    ))}
-                
-                </ul>
-            </nav>
+                    
+                    </SidebarMenuButton>
+                </SidebarMenuItem>
             
-            <div className="p-4 border-t border-primary-800">
-                <div className="flex items-center">
-                    
-                    <Image src="https://randomuser.me/api/portraits/women/46.jpg"
-                           alt="Admin"
-                           quality={85}
-                           width={120}
-                           height={120}
-                           className="w-10 h-10 rounded-full"/>
-                    
-                    <div className="ml-3">
-                        <p className="font-medium">Current User</p>
-                        <p className="text-sm text-primary-400">123@312.com</p>
-                    </div>
-                    
-                    <a href="#" className="ml-auto text-primary-400 hover:text-white">
-                        <i className="fas fa-sign-out-alt"></i>
-                    </a>
-                
-                </div>
-            </div>
-        </div>
+            
+            ))}
+        </>
     )
 }
 
@@ -93,14 +140,15 @@ function SideBarItems({ItemIcon, itemLabel, OnClick, bIsActive}: {
     OnClick: (ev: React.PointerEvent, label: string) => void,
     bIsActive: boolean
 }) {
+    
     return (
-        <li>
-            <Link href="/users"
-                  onPointerDown={(ev) => OnClick(ev, itemLabel)}
-                  className={`${bIsActive && 'activeItem'} flex items-center p-3 text-primary-200 hover:bg-primary-800 rounded-lg [&.activeItem]:border-l-4 [&.activeItem]:border-l-secondary-500 [&.activeItem]:bg-secondary-800`}>
-                <ItemIcon className="h-6 w-6 mr-3"/>
-                <span>{itemLabel}</span>
-            </Link>
-        </li>
+        
+        <Link href="/users"
+              onPointerDown={(ev) => OnClick(ev, itemLabel)}
+              className={`${bIsActive && 'activeItem'} grow flex items-center p-3 text-primary-200 hover:bg-primary-800 rounded-lg [&.activeItem]:border-l-4 [&.activeItem]:border-l-secondary-500 [&.activeItem]:bg-secondary-800`}>
+            <ItemIcon className={`size-6 mr-3 shrink-0 `}/>
+            <span>{itemLabel}</span>
+        </Link>
+    
     )
 }
